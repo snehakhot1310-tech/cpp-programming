97cf672d0099ec95c7a7ba06b4a800fd3504450a
snehakhot1310-tech <snehakhot1310@gmail.com>
2026-07-24 09:02:26 +0530

    Add files via upload

---
+++ Mult.c
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"enter two numbers:";
+cin>>a>>b;
+cout<<"add="<<a+b<<endl;
+cout<<"sub="<<a-b<<endl;
+cout<<"mult="<<a*b<<endl;
+cout<<"div="<<a/b<<endl;
+cout<<"mod="<<a%b<<endl;
+}
